ba985007f4784ed60cc4d17e9a03f4c2d723afc4
Thang <nguyenthangnnh@outlook.com>
2024-10-10 21:07:11 +0700

    Đếu FIx Được BUG

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const { addMovie, updateMovie, deleteMovie, getMovies } = require('../controllers/movieController');
 const router = express.Router();
+const movieController = require('../controllers/movieController');
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
 
-router.post('/', addMovie);
-router.put('/:id', updateMovie);
-router.delete('/:id', deleteMovie);
-router.get('/', getMovies);
+// Lấy danh sách phim
+router.get('/', movieController.getMovies);
+
+// Thêm phim
+router.post('/', upload.single('movieImage'), movieController.addMovie); // Chỉnh sửa đường dẫn thành '/' để khớp với router
+
+// Cập nhật phim
+router.put('/:id', upload.single('movieImage'), movieController.updateMovie);
+
+
+// Xóa phim
+router.delete('/:id', movieController.deleteMovie);
 
 module.exports = router;
